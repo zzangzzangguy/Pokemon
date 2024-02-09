dde784fbb49ce46149a52d71f1be8d7f5065edf0
camosss <camosss@naver.com>
2024-02-09 17:41:45 +0900

    docs: Application 폴더링

---
+++ Pokemon/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Pokemon
+//
+//  Created by 강호성 on 2/9/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
+
---
+++ Pokemon/Application/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Pokemon
+//
+//  Created by 강호성 on 2/9/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let _ = (
+            scene as? UIWindowScene
+        ) else {
+            return
+        }
+    }
+}
+
