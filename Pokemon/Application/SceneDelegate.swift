07793e36ed783ed7c4caed85e989541409fd4941
camosss <camosss@naver.com>
2024-02-09 17:49:12 +0900

    docs: main storyboard 제거

--- Pokemon/Application/SceneDelegate.swift
+++ Pokemon/Application/SceneDelegate.swift
@@ -16,11 +16,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        guard let _ = (
-            scene as? UIWindowScene
-        ) else {
-            return
-        }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = UINavigationController(
+            rootViewController: ViewController()
+        )
+        window?.makeKeyAndVisible()
     }
 }
 
