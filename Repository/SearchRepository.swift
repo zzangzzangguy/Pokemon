53659136a48e657123274561b49d2d5eace3fb5a
김기현 <creeps1104@gmail.com>
2024-02-12 18:01:43 +0900

    feat: 검색기능 추가

--- Repository/SearchRepository.swift
+++ Repository/SearchRepository.swift
@@ -1,12 +1,35 @@
+
 //
-//  SearchRepository.swift
+//  SearchRepositoryExtension+.swift
 //  Pokemon
 //
 //  Created by 김기현 on 2/12/24.
 //
 
 import Foundation
+import Moya
 
-protocol SearchRepository {
-    func searchCards(with query: String, completion: @escaping (Result<[PokemonCard], Error>) -> Void)
+class DefaultSearchRepository: SearchRepository {
+    private let provider: MoyaProvider<PokemonAPI>
+
+    init(provider: MoyaProvider<PokemonAPI> = MoyaProvider<PokemonAPI>()) {
+        self.provider = provider
+    }
+
+    func searchCards(with query: String, completion: @escaping (Result<[PokemonCard], Error>) -> Void) {
+        provider.request(.fetchCards(query: query)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let cardsDataModels = try response.map(PokemonCardResponse.self).data
+                    let cards = cardsDataModels.map { $0.toDomain() }
+                    completion(.success(cards))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
 }
