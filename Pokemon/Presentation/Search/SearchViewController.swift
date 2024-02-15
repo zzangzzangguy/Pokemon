898df9d6c65b6e98734eec520fb416241e96790c
camosss <camosss@naver.com>
2024-02-15 22:22:55 +0900

    feat: List/Search ViewController 생성

---
+++ Pokemon/Presentation/Search/SearchViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SearchViewController.swift
+//  Pokemon
+//
+//  Created by 강호성 on 2/15/24.
+//
+
+import UIKit
+
+final class SearchViewController: BaseViewController {
+
+    // MARK: - Properties
+
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Helpers
+
+    override func setView() {
+        super.setView()
+    }
+
+    override func setConstraints() {
+        super.setConstraints()
+    }
+
+    override func setConfiguration() {
+        super.setConfiguration()
+    }
+}
