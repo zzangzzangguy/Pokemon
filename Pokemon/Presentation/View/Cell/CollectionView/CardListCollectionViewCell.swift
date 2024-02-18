56c1148e9f1ced67483c23d2d406d94048da14cc
camosss <camosss@naver.com>
2024-02-18 23:19:15 +0900

    feat: Base CollectionviewCell/TableViewCell

---
+++ Pokemon/Presentation/View/Cell/CollectionView/CardListCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  CardListCollectionViewCell.swift
+//  Pokemon
+//
+//  Created by 강호성 on 2/18/24.
+//
+
+import UIKit
+
+final class CardListCollectionViewCell: BaseCollectionViewCell<PokemonCard> {
+
+    // MARK: - Properties
+
+
+    // MARK: - Helpers
+
+    override func setView() {
+        super.setView()
+    }
+
+    override func setConstraints() {
+        super.setConstraints()
+    }
+
+    override func setConfiguration() {
+        super.setConfiguration()
+    }
+
+    override func bind(_ model: PokemonCard?) {
+        super.bind(model)
+    }
+}
