18337230950ac7fc7734a4a7d811e34387e5bd00
김기현 <creeps1104@gmail.com>
2024-02-21 20:58:50 +0900

    mod: 에러처리 개선 및 코드구조 개선

--- Pokemon/Presentation/Search/SearchReactor.swift
+++ Pokemon/Presentation/Search/SearchReactor.swift
@@ -9,85 +9,59 @@ import ReactorKit
 import RxSwift
 
 class SearchReactor: Reactor {
-    enum Action {
-        case updateSearchQuery(String)
-        case search
-        case cancel
-    }
-
-    enum Mutation {
-        case setQuery(String)
-        case setSearchResults([PokemonCard])
-        case setLoading(Bool)
-        case setError(Error)
-    }
-
-    struct State {
-        var query: String = ""
-        var searchResults: [PokemonCard] = []
-        var isLoading: Bool = false
-        var error: Error?
-    }
-
     let initialState = State()
     private let pokemonRepository: PokemonRepositoryType
 
     init(pokemonRepository: PokemonRepositoryType) {
-          self.pokemonRepository = pokemonRepository
-      }
+        self.pokemonRepository = pokemonRepository
+    }
 
+    enum Action {
+        case updateSearchQuery(String)
+        case search
+    }
+
+    enum Mutation {
+        case setQuery(String)
+        case setSearchResults(Result<[PokemonCard], Error>)
+    }
+
+    struct State {
+        var query: String = ""
+        var searchResult: Result<[PokemonCard], Error>?
+    }
 
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
         case .updateSearchQuery(let query):
             return Observable.just(Mutation.setQuery(query))
-
         case .search:
-            return Observable.concat([
-                Observable.just(Mutation.setLoading(true)),
-                searchQuery().map(Mutation.setSearchResults),
-                Observable.just(Mutation.setLoading(false))
-            ]).catchError { Observable.just(Mutation.setError($0)) }
-
-        case .cancel:
-            return .empty()
+            return searchQuery().map(Mutation.setSearchResults)
         }
     }
+
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
         switch mutation {
         case .setQuery(let query):
             newState.query = query
-
-        case .setSearchResults(let results):
-            newState.searchResults = results
-
-        case .setLoading(let isLoading):
-            newState.isLoading = isLoading
-
-        case .setError(let error):
-            newState.error = error
+        case .setSearchResults(let result):
+            newState.searchResult = result
         }
         return newState
     }
 
-    private func searchQuery() -> Observable<[PokemonCard]> {
-        
+    private func searchQuery() -> Observable<Result<[PokemonCard], Error>> {
         return Observable.create { [weak self] observer in
-                   guard let self = self else { return Disposables.create() }
+            guard let self = self else { return Disposables.create() }
 
-                   let request = CardsRequest(query: self.currentState.query)
-                   self.pokemonRepository.fetchCards(request: request) { result in
-                       switch result {
-                       case .success(let data):
-                           observer.onNext(data)
-                           observer.onCompleted()
-                       case .failure(let error):
-                           observer.onError(error)
-                       }
-                   }
+            let request = CardsRequest(query: self.currentState.query)
+            self.pokemonRepository.fetchCards(request: request) { result in
+                observer.onNext(result)
+                observer.onCompleted()
+            }
 
-                   return Disposables.create()
-               }
-           }
-       }
+            return Disposables.create()
+        }
+    }
+}
