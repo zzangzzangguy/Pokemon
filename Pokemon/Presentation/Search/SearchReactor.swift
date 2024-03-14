a58e66f2767bb39054eb84f303e70cd91b4be5ec
김기현 <creeps1104@gmail.com>
2024-03-15 04:34:00 +0900

    mod: 텍스트필드 타임아웃 해결

--- Pokemon/Presentation/Search/SearchReactor.swift
+++ Pokemon/Presentation/Search/SearchReactor.swift
@@ -3,69 +3,78 @@ import RxSwift
 
 class SearchReactor: Reactor {
     var initialState = State()
-
+    
     private let pokemonRepository: PokemonRepositoryType
-
+    
     init(pokemonRepository: PokemonRepositoryType) {
         self.pokemonRepository = pokemonRepository
     }
-
+    
     enum Action {
         case updateSearchQuery(String)
         case search(String)
         case loadNextPage
     }
-
+    
     enum Mutation {
         case setQuery(String)
         case setSearchResults([PokemonCard])
         case appendSearchResults([PokemonCard])
         case setLoading(Bool)
         case setCanLoadMore(Bool)
+        case setNoResults(Bool)
+        
         
     }
-
+    
     struct State {
         var query: String = ""
         var searchResult: [PokemonCard] = []
         var isLoading: Bool = false
         var canLoadMore: Bool = true
+        var noResults: Bool = false
+        
     }
-
+    
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
         case .updateSearchQuery(let query):
-            return .just(.setQuery(query))
-
+            return .concat([
+                .just(.setQuery(query)),
+                .just(.setNoResults(false))
+            ])
+            
+            
         case .search(let query):
             guard !query.isEmpty else {
                 return .concat([
                     .just(.setLoading(false)),
-                    .just(.setSearchResults([]))
+                    .just(.setSearchResults([])),
+                    .just(.setNoResults(true))
                 ])
             }
-
+            
             let initialPage = 1
-
+            
             return .concat([
-                       .just(.setLoading(true)),
-                       searchQuery(query: query, page: initialPage)
-                           .map { results in
-                               if results.isEmpty {
-                                   return .setSearchResults([])
-                               } else {
-                                   return .setSearchResults(results)
-                               }
-                           },
-                       .just(.setLoading(false))
-                   ])
+                .just(.setLoading(true)),
+                searchQuery(query: query, page: initialPage)
+                    .map { results in
+                        if results.isEmpty {
+                            return .setNoResults(true)
+                        } else {
+                            return .setSearchResults(results)
+                        }
+                    },
+                .just(.setLoading(false))
+            ])
         case .loadNextPage:
             guard !currentState.isLoading, currentState.canLoadMore else {
                 return .empty()
             }
-
+            
             let nextPage = (currentState.searchResult.count / 20) + 1
-
+            
             return .concat([
                 .just(.setLoading(true)),
                 searchQuery(query: currentState.query, page: nextPage)
@@ -75,35 +84,38 @@ class SearchReactor: Reactor {
             ])
         }
     }
-
+    
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
-
+        
         switch mutation {
         case .setQuery(let query):
             newState.query = query
-
+            
         case .setSearchResults(let result):
             newState.searchResult = result
             newState.canLoadMore = true
-
+            
         case .appendSearchResults(let results):
             newState.searchResult += results
-
+            
         case .setLoading(let isLoading):
             newState.isLoading = isLoading
-
+            
         case .setCanLoadMore(let canLoadMore):
             newState.canLoadMore = canLoadMore
+        case .setNoResults(let noResults):
+            newState.noResults = noResults
+            
         }
-
+        
         return newState
     }
-
+    
     private func searchQuery(query: String, page: Int) -> Observable<[PokemonCard]> {
         let pageSize = 20
         let request = CardsRequest(query: query, page: page, pageSize: pageSize)
-
+        
         return Observable.create { observer in
             self.pokemonRepository.fetchCards(request: request) { result in
                 switch result {
@@ -112,10 +124,10 @@ class SearchReactor: Reactor {
                 case .failure:
                     observer.onNext([])
                 }
-
+                
                 observer.onCompleted()
             }
-
+            
             return Disposables.create()
         }
     }
--- Pokemon/Presentation/Search/SearchViewController.swift
+++ Pokemon/Presentation/Search/SearchViewController.swift
@@ -22,13 +22,13 @@ final class SearchViewController: BaseViewController, ReactorKit.View {
         indicator.hidesWhenStopped = true
         return indicator
     }()
-
+    
     // MARK: - Initialization
     required init(reactor: SearchReactor) {
         super.init()
         self.reactor = reactor
     }
-
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,24 +36,24 @@ final class SearchViewController: BaseViewController, ReactorKit.View {
         setConstraints()
         setConfiguration()
     }
-
+    
     // MARK: - Binding
     func bind(reactor: SearchReactor) {
         // Action
         searchController.searchBar.rx.text.orEmpty
             .distinctUntilChanged()
-            .debounce(.milliseconds(500), scheduler: MainScheduler.instance)
+        //            .debounce(.milliseconds(500), scheduler: MainScheduler.instance)
             .map { Reactor.Action.updateSearchQuery($0) }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
-
+        
         searchController.searchBar.rx.searchButtonClicked
             .withLatestFrom(searchController.searchBar.rx.text.orEmpty)
             .filter { !$0.isEmpty }
             .map { Reactor.Action.search($0) }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
-
+        
         tableView.rx.contentOffset
             .filter { [weak self] offset in
                 guard let self = self else { return false }
@@ -62,47 +62,58 @@ final class SearchViewController: BaseViewController, ReactorKit.View {
             .map { _ in Reactor.Action.loadNextPage }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
-
+        
         // State
         reactor.state.map { $0.isLoading }
             .distinctUntilChanged()
             .bind(to: loadingIndicator.rx.isAnimating)
             .disposed(by: disposeBag)
-
+        
         reactor.state.map { !$0.isLoading }
             .distinctUntilChanged()
             .bind(to: loadingIndicator.rx.isHidden)
             .disposed(by: disposeBag)
-
+        
         reactor.state.map { $0.searchResult }
             .bind(to: tableView.rx.items(cellIdentifier: PokemonCardTableViewCell.reuseIdentifier, cellType: PokemonCardTableViewCell.self)) { _, item, cell in
                 cell.configure(with: item)
             }
             .disposed(by: disposeBag)
-
-        reactor.state.map { $0.searchResult.isEmpty }
-                .distinctUntilChanged()
-                .filter { $0 }
-                .subscribe(onNext: { [weak self] _ in
+        
+        //        reactor.state.map { $0.searchResult.isEmpty }
+        //                .distinctUntilChanged()
+        //                .filter { $0 }
+        //                .subscribe(onNext: { [weak self] _ in
+        //                    self?.searchController.searchBar.resignFirstResponder()
+        //                    self?.showNoResultsAlert()
+        //                })
+        //                .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.noResults }
+            .distinctUntilChanged()
+            .bind(onNext: { [weak self] noResults in
+                if noResults {
                     self?.searchController.searchBar.resignFirstResponder()
                     self?.showNoResultsAlert()
-                })
-                .disposed(by: disposeBag)
-        }
-
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    
     // MARK: - Helpers
     override func setView() {
         super.setView()
-
+        
         searchController.searchBar.placeholder = " 포켓몬을 검색하세요!"
         searchController.obscuresBackgroundDuringPresentation = false
         navigationItem.searchController = searchController
-
+        
         tableView.delegate = nil
         view.addSubview(tableView)
         view.addSubview(loadingIndicator)
     }
-
+    
     override func setConstraints() {
         super.setConstraints()
         tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
@@ -110,19 +121,22 @@ final class SearchViewController: BaseViewController, ReactorKit.View {
             $0.center.equalToSuperview()
         }
     }
-
+    
     override func setConfiguration() {
         super.setConfiguration()
         tableView.register(PokemonCardTableViewCell.self, forCellReuseIdentifier: PokemonCardTableViewCell.reuseIdentifier)
         tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        
+        searchController.searchBar.autocorrectionType = .no
+        searchController.searchBar.spellCheckingType = .no
     }
-
+    
     private func showNoResultsAlert() {
         let alertController = UIAlertController(title: "검색 결과 없음", message: "입력한 검색어에 해당하는 포켓몬이 없습니다.", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
-
+    
     private func showErrorAlert(message: String) {
         let alertController = UIAlertController(title: "에러", message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
